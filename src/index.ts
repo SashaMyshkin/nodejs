8a410077a58af8d0ccd415d364bf09587f62e774
alexandros <homocalidus@gmail.com>
2023-10-19 15:35:01 +0200

    Revert "separate routes and search"
    This reverts commit 0eacc62ec447e4a3d667e692ef0e9fed5c954d3f.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,46 @@
-import express, { Express } from "express";
-import todosRouter from "./routes/todos";
+import express, { Express, Request, Response } from "express";
+
+import Todo from './types/Todo';
+import Database from "./database";
 
 const app: Express = express();
-const port: number = 8080;
+const port: number = 3000;
 
 app.use(express.json());
 
-//Don't forget following slash!!!
-app.use('/api/todos', todosRouter);
+
+app.get("/todos", (req: Request, res: Response) => {
+	const database = new Database<Todo>('todos');
+	const data = database.selectAll();
+	res.send(JSON.stringify(data));
+});
+
+app.get("/todos/:id", (req: Request, res: Response) => {
+	const id = Number(req.params.id);
+	const database = new Database<Todo>('todos');
+	const data = database.select(id);
+	res.send(JSON.stringify(data));
+});
+
+app.post("/todos", (req: Request, res: Response) => {
+	const database = new Database<Todo>('todos');
+	const data = database.insert(req.body);
+	res.send(JSON.stringify(data));
+});
+
+app.put("/todos", (req: Request, res: Response) => {
+	const database = new Database<Todo>('todos');
+	const data = database.update(req.body);
+	res.send(JSON.stringify(data));
+});
+
+app.delete("/todos/:id", (req: Request, res: Response) => {
+	const id = Number(req.params.id);
+	const database = new Database<Todo>('todos');
+	const data = database.delete(id);
+	res.send(JSON.stringify(data));
+});
+
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
