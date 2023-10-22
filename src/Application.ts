1224f6b5662364958f8fefc41946f86bd6d0c8fc
Alexander Yovanovitch <78900309+SashaMyshkin@users.noreply.github.com>
2023-10-22 00:02:29 +0000

    Application.ts removed from git

--- src/Application.ts
+++
@@ -1,6 +0,0 @@
-class Application {
-    static SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVobWZidmpleWxodnVqc3h0ZXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc3MjM4MDYsImV4cCI6MjAxMzI5OTgwNn0.PwXc89-UEMB-v_QVYe_hdHqeyviJuCrJ5ISv7f0BTLI";
-    static SUPABASE_URL = "https://ehmfbvjeylhvujsxteug.supabase.co";
-}
-
-export default Application;
