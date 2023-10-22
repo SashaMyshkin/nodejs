171f9ad3f09bccc50fadbba462921f7bc0394aaf
Alexander Yovanovitch <78900309+SashaMyshkin@users.noreply.github.com>
2023-10-22 00:22:19 +0000

    whatever

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import Application from './Application';
+import Application from '../Application';
 
 const database = createClient(Application.SUPABASE_URL, Application.SUPABASE_KEY);
 //const database = createClient(process.env["SUPABASE_URL"]!, process.env["SUPABASE_KEY"]!);
